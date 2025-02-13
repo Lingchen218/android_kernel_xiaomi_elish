5a1ca9a52a8a9da24972f1f41758467da162b82c
lingchen218 <lichen218@qq.com>
2025-02-13 09:10:52 +0800

    build

--- drivers/tty/tty_jobctrl.c
+++ drivers/tty/tty_jobctrl.c
@@ -525,9 +525,6 @@ static int tiocgsid(struct tty_struct *tty, struct tty_struct *real_tty, pid_t _
 	unsigned long flags;
 	pid_t sid;
 
-	unsigned long flags;
-	pid_t sid;
-
 	/*
 	 * (tty == real_tty) is a cheap way of
 	 * testing if the tty is NOT a master pty.
@@ -536,18 +533,7 @@ static int tiocgsid(struct tty_struct *tty, struct tty_struct *real_tty, pid_t _
 		return -ENOTTY;
 
 	spin_lock_irqsave(&real_tty->ctrl_lock, flags);
-
-	spin_lock_irqsave(&real_tty->ctrl_lock, flags);
 	if (!real_tty->session)
-		goto err;
-	sid = pid_vnr(real_tty->session);
-	spin_unlock_irqrestore(&real_tty->ctrl_lock, flags);
-
-	return put_user(sid, p);
-
-err:
-	spin_unlock_irqrestore(&real_tty->ctrl_lock, flags);
-	return -ENOTTY;
 		goto err;
 	sid = pid_vnr(real_tty->session);
 	spin_unlock_irqrestore(&real_tty->ctrl_lock, flags);
